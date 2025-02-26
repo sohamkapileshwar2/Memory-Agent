af5f3d9b61dc8f4c1a005ac43e82f0028a86eed7
neversettle17-101 <aditi1178@gmail.com>
2025-02-27 00:12:50 +0530

    tool calling from llm response

--- src/example.py
+++ src/example.py
@@ -1,5 +1,7 @@
 from llm.gemini import gemini_with_tools
 from memory.core_memory import agent_memory
+from tools.store_into_memory import store_into_memory
+
 
 system_prompt = "You are a chatbot. " \
 + "You have a section of your context called [MEMORY] " \
@@ -14,8 +16,9 @@ messages = [
     ("human", "I love programming."),
 ]
 
-ai_msg = gemini_with_tools.invoke(messages)
-# print(ai_msg)
-print("\n\n\n")
-print("CONTENT", ai_msg.content)
-print("AGENT MEMORY", agent_memory)
+gemini_response = gemini_with_tools.invoke(messages)
+
+tool_call = gemini_response.tool_calls[0]
+tool_output = {"store_into_memory": store_into_memory}[tool_call["name"].lower()].invoke(tool_call["args"])
+
+print(agent_memory)
--- src/llm/gemini.py
+++ src/llm/gemini.py
@@ -12,12 +12,12 @@ class CustomHandler(BaseCallbackHandler):
         self, serialized: dict[str, any], prompts: list[str], **kwargs: any
     ) -> any:
         formatted_prompts = "\n".join(prompts)
-        print(f"On LLM Start:\n{formatted_prompts}")
+        # print(f"On LLM Start:\n{formatted_prompts}")
     
-    def on_tool_start( self, serialized: dict[str, any], input_str: str, **kwargs: any
-    ) -> any:
-        formatted_prompts = "\n".join(input_str)
-        print(f"On Tool start:\n{formatted_prompts}")
+    # def on_tool_start( self, serialized: dict[str, any], input_str: str, **kwargs: any
+    # ) -> any:
+    #     formatted_prompts = "\n".join(input_str)
+    #     print(f"On Tool start:\n{formatted_prompts}")
 
 gemini = ChatGoogleGenerativeAI(model="gemini-2.0-flash",temperature=0,max_tokens=None,timeout=30,max_retries=2,callbacks=[CustomHandler()])
 gemini_with_tools = gemini.bind_tools(tool_calls)
