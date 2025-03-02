1e452fe5ebcea4286d22f6c8ca18690fbc24df28
Soham Kapileshwar <soham@Sohams-MacBook-Pro.local>
2025-03-02 12:30:13 +0530

    Persist to memory

--- src/tools/store_into_memory.py
+++ src/tools/store_into_memory.py
@@ -1,8 +1,24 @@
 from langchain_core.tools import tool
-from memory.core_memory import agent_memory
 
-@tool
-def store_into_memory(user_type:str, attribute:str, info:str):
+from pydantic import BaseModel, Field
+
+# Store into memory tool call definition using pydantic class
+# Used to pass definition of the function to LLM
+class StoreIntoMemory(BaseModel):
+    '''
+    Stores information inputed by the user for the particular attribute in core memory.
+
+    Args:
+        user_type: It can be either human or agent
+        attribute: Attribute/Key name of the information of the user
+        info: Information to be stored corresponsing to the attribute
+    '''
+    user_type: str = Field(description="Enum: human | agent")
+    attribute: str = Field(description="Attribute name of the information being stored about the user type")
+    info: str = Field(description="Information being stored corresponding to the attribute")
+
+# Store into memory function implementation
+def store_into_memory(user_type:str, attribute:str, info:str, agent_memory:dict):
     '''
     Stores information inputed by the user for the particular attribute in core memory.
 
@@ -16,7 +32,4 @@ def store_into_memory(user_type:str, attribute:str, info:str):
     else:
         agent_memory[user_type] = {}
         agent_memory[user_type][attribute]=info
-
-tool_calls = [store_into_memory]
-
 
